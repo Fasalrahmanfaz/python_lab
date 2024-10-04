b90025b766f30b569cf06a88c826c9824786b6d0
Fasalrahmanfaz <fasalrahmankalappattil@gmail.com>
2024-10-04 12:10:57 +0530

    Add files via upload

---
+++ first month/27-09-2024/incrementanddecrement.py
@@ -0,0 +1,4 @@
+a=10
+print("value of a",a)
+print("increment 5=",a+5)
+print("decrement 3=",a+5-3)
---
+++ first month/27-09-2024/triangle.py
@@ -0,0 +1,3 @@
+h = int (input(" enter the hight of a triangle"))
+b = int ( input("enter the base of a triangle"))
+print("area of a triangle:",1/2*b*h)
