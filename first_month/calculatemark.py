b197fc0c5412174ba3bbc249a11ac829889f00cf
Fasalrahmanfaz <fasalrahmankalappattil@gmail.com>
2024-10-04 12:06:12 +0530

    Delete first_month/calculatemark.py

--- first_month/calculatemark.py
+++
@@ -1,9 +0,0 @@
-sub1= int(input("enter mark of sub1"))
-sub2= int(input("enter mark of sub2"))
-sub3= int(input("enter mark of sub3"))
-sub4= int(input("enter mark of sub4"))
-sub5= int(input("enter mark of sub5"))
-total=sub1+sub2+sub3+sub4+sub5
-print("the total mark",total)
-print("percentage",total/250*100)
-
