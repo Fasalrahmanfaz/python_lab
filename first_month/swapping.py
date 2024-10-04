3958872d3c795bcb0fcb35e44343bbb8e6ba4cd7
Fasalrahmanfaz <fasalrahmankalappattil@gmail.com>
2024-10-04 12:05:15 +0530

    Delete first_month/swapping.py

--- first_month/swapping.py
+++
@@ -1,7 +0,0 @@
-num1= int(input("enter num1"))
-num2= int(input("enter num2"))
-print("before swapping",num1,num2)
-num1=num1+num2  #num1=5 num2=10 5+10=15
-num2=num1-num2  #num1=15 15-10=5
-num1=num1-num2
-print("swapping result",num1,num2)
