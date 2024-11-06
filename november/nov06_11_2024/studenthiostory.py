675493fc68a6fbd6e93bb512852aa19a7d953a9e
fasal rahman <fasalrahmankalappattil@gmail.com>
2024-11-06 15:29:13 +0530

    Add files via upload

---
+++ november/nov06_11_2024/studenthiostory.py
@@ -0,0 +1,30 @@
+student = {
+    "name": input("Enter student name: "),
+    "roll.no": input("Enter roll number of the student: "),
+    "reg.no.": input( "Enter register number: "),
+    "department.": input("Enter department name: "),
+    "sem.": input("Enter the semester:"),
+}
+print(student)
+student.update({"total_marks":int(input("Enter total marks: "))})
+print("After adding total marks:")
+print(student)
+def calculate_grade(total_marks):
+    if total_marks >= 90:
+        return 'A'
+    elif total_marks >= 82:
+        return 'B'
+    elif total_marks >= 75:
+        return 'C'
+    elif total_marks >= 60:
+        return 'D'
+    elif total_marks >= 50:
+        return 'P'
+    else:
+        return 'F'
+student['grade'] = calculate_grade(student['total_marks'])
+print("After adding grade:")
+print(student)
+print("After deleting roll number:")
+del student['roll.no']
+print(student)
